ad61b6cbd1ebae9298cd74abd7491cf044b3c109
unknown <tdunmire@RTI-025267.RTI.NS>
2015-06-17 16:33:10 -0400

    adding more lines to chpt1

--- chpt1.py
+++ chpt1.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from collections import Counter
 
 #Data dump of users
 users = [
@@ -43,4 +44,33 @@ num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
 
 sorted(num_friends_by_id, key = lambda (user_id, num_friends): num_friends, reverse = True)
 
-#pg 6 pickup
+#Define friends of friends
+
+def friends_of_friend_ids_bad(user):
+    return [foaf["id"]
+        for friend in user["friends"] #for each of user's friends
+        for foaf in friend["friends"]] #get each of their friends
+
+def not_the_same(user, other_user):
+    #two users are not hte same if they have different ids
+    return user["id"] != other_user["id"]
+
+def not_friends(user, other_user):
+    #other_user is not a friend if he's not in user["friends"]
+    return all(not_the_same(friend, other_user)
+        for friend in user["friends"])
+
+def friends_of_friend_ids(user):
+    return Counter(foaf["id"]
+                    for friend in user["friends"] #for each of my friends
+                    for foaf in friend["friends"] #count *their* friends
+                    if not_the_same(user, foaf)   #who aren't me
+                    and not_friends(user, foaf))  #and aren't my friends
+
+print friends_of_friend_ids(users[3]) #Counter({0: 2, 5: 1})
+
+interests = [ #pick up page 7
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+
+]
